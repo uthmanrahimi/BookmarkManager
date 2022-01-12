7274c70cc5c56b68f436a2856c9c8b4aa09bd97a
Uthman <uthmanrahimi@gmail.com>
2022-01-12 17:26:43 +0330

    remove comments

--- src/BookmarkManager.API/Startup.cs
+++ src/BookmarkManager.API/Startup.cs
@@ -24,7 +24,6 @@ namespace BookmarkManager.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructure(Configuration);
@@ -38,7 +37,6 @@ namespace BookmarkManager.API
             services.AddSingleton<ICurrentUserService, CurrentUserService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
