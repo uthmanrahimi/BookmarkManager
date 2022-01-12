e48fd9cf5db92b6fcdc27eef6674eeb41822522e
Uthman <uthmanrahimi@gmail.com>
2022-01-12 11:12:43 +0330

    registered Infrastructure's services with an extension method

--- src/BookmarkManager.API/Startup.cs
+++ src/BookmarkManager.API/Startup.cs
@@ -1,16 +1,11 @@
+using BookmarkManager.Infrastructure;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookmarkManager.API
 {
@@ -26,7 +21,7 @@ namespace BookmarkManager.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddInfrastructure(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ src/BookmarkManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using BookmarkManager.Application.Interfaces;
+using BookmarkManager.Infrastructure.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookmarkManager.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+                services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("bookmarkmg"));
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            }
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+            return services;
+
+        }
+    }
+}
